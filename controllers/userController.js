6c24cebda4d5d4a05b1ffc30cba6e2006e11c14b
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-07-18 17:58:38 +0300

    user._id and not user.user._id

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,7 +36,7 @@ const signupUser = async (req, res) => {
         });
 
         const token = createToken(user._id);
-        res.status(200).json({email, token, role: user.role, userId: user.user._id});
+        res.status(200).json({email, token, role: user.role, userId: user._id});
 
     } catch(error) {
         res.status(400).json({error: error.message});
