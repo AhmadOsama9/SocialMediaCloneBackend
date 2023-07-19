38c61a366a4cb39f5ad56f7f0dd30e0452987cf3
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-07-19 12:48:26 +0300

    f**k forgot to return user_id in login

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ const loginUser = async (req, res) => {
 
         const token = createToken(user._id);
 
-        res.status(200).json({email, token, role: user.role});
+        res.status(200).json({email, token, role: user.role, userId: user._id});
 
     } catch (error) {
         res.status(400).json({error: error.message});
