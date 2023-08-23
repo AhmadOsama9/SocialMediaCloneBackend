5adc91b18485c0af8673694f8804a3e9abfe6fbd
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-08-23 22:52:16 +0300

    test

--- server.js
+++ server.js
@@ -21,11 +21,14 @@ app.use(express.json());
 
 app.use(passport.initialize());
 
-
 app.use((req, res, next) => {
     console.log(req.path, req.method);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Include the methods you need
     next();
-});
+  });
+  
 
 app.use("/api/user/", userRoutes);
 app.use("/api/user/", profileRoutes);
