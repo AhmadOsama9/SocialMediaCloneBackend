8d9f86fed3e9742f01cd8e7d9b15cb9ec57ae742
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-08-24 18:59:00 +0300

    remove googeSingup controller function

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,5 +78,4 @@ module.exports = {
     signupUser,
     loginUser,
     googleLogin,
-    googleSignup,
 };
--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,6 @@ const {
     signupUser, 
     loginUser,
     googleLogin,
-    googleSignup
 } = require("../controllers/userController");
 
 const router = express.Router();
