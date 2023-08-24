e24ce73a111cb827e112c495642db0201e57dca2
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-08-24 18:24:36 +0300

    back to normal

--- routes/user.js
+++ routes/user.js
@@ -20,6 +20,7 @@ router.get(
 
 router.get(
     "/auth/google/signup/callback",
+    passport.authenticate("google-signup", { failureRedirect: "http://localhost:5173/" }),
     googleSignup
 );
   
