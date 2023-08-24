34564e4cb0d2912ed21d2de363373198daeae49d
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-08-24 18:58:22 +0300

    test

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const passport = require("../passport");
+const User = require("../models/userModel");
+const Profile = require("../models/profileModel");
+const UserActivity = require("../models/userActivityModel");
 
 const { 
     signupUser, 
@@ -21,7 +24,42 @@ router.get(
 router.get(
     "/auth/google/signup/callback",
     passport.authenticate("google-signup", { failureRedirect: "http://localhost:5173/" }),
-    googleSignup
+    async function (req, res) {
+        console.log("It enters the googleSignup that being called through callback of googleSignup");
+        try {
+            const profile = req.user;
+
+            if (!profile || !profile.emails || !profile.emails[0] || !profile.emails[0].value) {
+                return res.status(400).json({ error: "Google profile or email not found" });
+            }
+
+            const email = profile.emails[0].value;
+
+            const userProfile = await Profile.create({
+                user: user._id,
+                nickname: "",
+                bio: "",
+            });
+            if (!userProfile) {
+                throw Error("Failed to create a profile");
+            }
+
+            const userActivity = await UserActivity.create({
+                user: user._id,
+            });
+            if (!userActivity) {
+                throw Error("Failed to create a userActivity");
+            }
+
+            const user = await User.googleSignup(email, "user");
+
+            const token = createToken(user._id);
+
+            res.status(200).json({ email, token, role: 'user', userId: user._id });
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
 );
   
 router.get(
