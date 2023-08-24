652a43af04ded0d95ec2ad1e0a4b1ecaf2484492
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-08-24 05:09:18 +0300

    disperate-test-11

--- passport.js
+++ passport.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: "https://merngymprojectbackend.onrender.com/api/user/auth/google/signup/callback",
+            callbackURL: "http://localhost:5173/signup",
             passReqToCallback   : true,
             scope: ["email"], // Only request email access
         },
