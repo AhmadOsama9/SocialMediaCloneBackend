ee8496b431be7282093baec225874f30935e3183
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-08-26 07:32:20 +0300

    back to using the googleSignup function

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,8 +74,46 @@ const signupUser = async (req, res) => {
     }
 }
 
+const googleSignup = async => (req, res) {
+    console.log("It enters the googleSignup that being called through callback of googleSignup");
+    try {
+        const profile = req.user;
+
+        if (!profile || !profile.emails || !profile.emails[0] || !profile.emails[0].value) {
+            return res.status(400).json({ error: "Google profile or email not found" });
+        }
+
+        const email = profile.emails[0].value;
+
+        const userProfile = await Profile.create({
+            user: user._id,
+            nickname: "",
+            bio: "",
+        });
+        if (!userProfile) {
+            throw Error("Failed to create a profile");
+        }
+
+        const userActivity = await UserActivity.create({
+            user: user._id,
+        });
+        if (!userActivity) {
+            throw Error("Failed to create a userActivity");
+        }
+
+        const user = await User.googleSignup(email, "user");
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token, role: 'user', userId: user._id });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
 module.exports = {
     signupUser,
     loginUser,
     googleLogin,
+    googleSignup,
 };
