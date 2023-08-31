0f5e46bb41fbd4c3b6434d66f4954192e353ee7d
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-09-01 02:18:46 +0300

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -49,7 +49,7 @@ const updateImage = async (req, res) => {
     const { userId, image } = req.body;
   
     try {
-      const image = await Profile.updateImage(userId, avatar);
+      const image = await Profile.updateImage(userId, image);
       res.status(200).json({ image });
     } catch (error) {
       res.status(400).json({ error: error.message });
