68ba58ed6db9099444e63ed4a3c7e07e4abc4fd2
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-09-07 19:06:03 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -137,7 +137,7 @@ async function verifyGoogleToken(accessToken) {
     try {
       // Make an HTTP GET request to Google's tokeninfo endpoint
       const response = await axios.get(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
-  
+        console.log("The error is in the callback the token is: ", accessToken);
       // Check if the response contains the required fields
       if (response.data.aud && response.data.aud === process.env.CLIENT_ID) {
         // The token is valid and intended for your application
